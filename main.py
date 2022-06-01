5377b6803ca2b7bbc029d9c863bb00daeed55bb9
Bad Hard <badhard5693@gmail.com>
2022-06-01 15:09:01 +0300

    <3

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates
 
 
-def parsing(date):
+def parsing_countries(date):
     response = urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + datetime.strftime(date, "%d/%m/%Y"))
     dom = xml.dom.minidom.parse(response)
     dom.normalize()
@@ -18,12 +18,12 @@ def parsing(date):
         childList = node.childNodes
         for child in childList:
             if child.nodeName == "Name":
-                countries.append(child.childNodes[0].nodeValue)
+                Countries.append(child.childNodes[0].nodeValue)
             if child.nodeName == "Value":
-                valutues.append(child.childNodes[0].nodeValue)
-    return countries,valutues
+                Valutues.append(child.childNodes[0].nodeValue)
+    return Countries, Valutues
 
-def kurs(country,date):
+def parsing_value(country, date):
     response = urllib.request.urlopen("http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + datetime.strftime(date, "%d/%m/%Y"))
     dom = xml.dom.minidom.parse(response)
     dom.normalize()
@@ -38,23 +38,23 @@ def kurs(country,date):
                             value = childd.childNodes[0].nodeValue
     return value
 
-def graf():
-    periodd = ch_period.get()
-    country = country_3.get()
-    if per.get()==1:
+def print_graf():
+    period = CB_Periods.get()
+    country = CB_Country3.get()
+    if RadioB_Sellector.get()==1:
         matplotlib.use('TkAgg')
         fig = matplotlib.pyplot.figure(figsize=(10, 4))
-        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig,master = grafic)
+        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master = Grafic)
         raz = timedelta(days = 1)
         plot_widget = canvas.get_tk_widget()
         fig.clear()
         x = []
         y = []
-        periodd=re.split(' - ',periodd)
-        temp1 = datetime.strptime(periodd[1],"%d/%m/%Y")
-        temp2 = datetime.strptime(periodd[0],"%d/%m/%Y")
+        period=re.split(' - ',period)
+        temp1 = datetime.strptime(period[1],"%d/%m/%Y")
+        temp2 = datetime.strptime(period[0],"%d/%m/%Y")
         while(temp1!=temp2+raz):
-            k = kurs(country,temp1)
+            k = parsing_value(country, temp1)
             k = k.replace(',','.')
             k = float(k)
             print(k)
@@ -66,20 +66,20 @@ def graf():
         matplotlib.pyplot.plot(x,y)
         matplotlib.pyplot.grid()
         plot_widget.grid(row = 5,column = 5)
-    if per.get() == 2:
+    if RadioB_Sellector.get() == 2:
         matplotlib.use('TkAgg')
         fig = matplotlib.pyplot.figure(figsize=(10, 4))
-        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=grafic)
+        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=Grafic)
         raz = timedelta(days=1)
         plot_widget = canvas.get_tk_widget()
         fig.clear()
         x = []
         y = []
-        periodd = re.split(' - ', periodd)
-        temp1 = datetime.strptime(periodd[1], "%d/%m/%Y")
-        temp2 = datetime.strptime(periodd[0], "%d/%m/%Y")
+        period = re.split(' - ', period)
+        temp1 = datetime.strptime(period[1], "%d/%m/%Y")
+        temp2 = datetime.strptime(period[0], "%d/%m/%Y")
         while (temp1 != temp2 + raz):
-            k = kurs(country, temp1)
+            k = parsing_value(country, temp1)
             k = k.replace(',', '.')
             k = float(k)
             if (datetime.strftime(temp1, "%d")) in x:
@@ -93,20 +93,20 @@ def graf():
         matplotlib.pyplot.plot(x, y)
         matplotlib.pyplot.grid()
         plot_widget.grid(row=5, column=5)
-    if per.get() == 3:
+    if RadioB_Sellector.get() == 3:
         matplotlib.use('TkAgg')
         fig = matplotlib.pyplot.figure(figsize=(10, 4))
-        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=grafic)
-        raz = timedelta(weeks = 3)
+        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=Grafic)
+        raz = timedelta(weeks = 2)
         plot_widget = canvas.get_tk_widget()
         fig.clear()
         x = []
         y = []
-        periodd = re.split(' - ', periodd)
-        temp1 = datetime.strptime(periodd[1], "%d/%m/%Y")
-        temp2 = datetime.strptime(periodd[0], "%d/%m/%Y")
+        period = re.split(' - ', period)
+        temp1 = datetime.strptime(period[1], "%d/%m/%Y")
+        temp2 = datetime.strptime(period[0], "%d/%m/%Y")
         while (temp1 < temp2):
-            k = kurs(country, temp1)
+            k = parsing_value(country, temp1)
             if k!=None:
                 k = k.replace(',', '.')
                 k = float(k)
@@ -117,20 +117,20 @@ def graf():
         matplotlib.pyplot.plot(x, y)
         matplotlib.pyplot.grid()
         plot_widget.grid(row=5, column=5)
-    if per.get() == 4:
+    if RadioB_Sellector.get() == 4:
         matplotlib.use('TkAgg')
         fig = matplotlib.pyplot.figure(figsize=(10, 4))
-        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=grafic)
+        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=Grafic)
         raz = timedelta(weeks=4.4)
         plot_widget = canvas.get_tk_widget()
         fig.clear()
         x = []
         y = []
-        periodd = re.split(' - ', periodd)
-        temp1 = datetime.strptime(periodd[1], "%d/%m/%Y")
-        temp2 = datetime.strptime(periodd[0], "%d/%m/%Y")
+        period = re.split(' - ', period)
+        temp1 = datetime.strptime(period[1], "%d/%m/%Y")
+        temp2 = datetime.strptime(period[0], "%d/%m/%Y")
         while (temp1 < temp2 + raz):
-            k = kurs(country, temp1)
+            k = parsing_value(country, temp1)
             k = k.replace(',', '.')
             k = float(k)
             x.append(datetime.strftime(temp1, "%b.%y"))
@@ -138,141 +138,130 @@ def graf():
             y.append(y1)
             temp1 += raz
         fig.clear()
-        matplotlib.pyplot.plot(x, y,'--')
+        matplotlib.pyplot.plot(x, y)
         matplotlib.pyplot.grid()
         plot_widget.grid(row=5, column=5)
 
 
 
-def period():
-    if per.get()==1:
-        periodscheck=[]
-        now = date.today()
-        temp = now
+def sellect_period():
+    periodscheck = []
+    now = date.today()
+    temp = now
+    flag = True
+
+    if RadioB_Sellector.get()==1:
         delta = timedelta(weeks = 1)
-        for i in range(4):
-            string = ''
-            string = temp.strftime("%d/%m/%Y") + ' - '
-            temp = temp - delta
-            string+=datetime.strftime(temp,"%d/%m/%Y")
-            periodscheck.append(string)
-        ch_period['values'] = periodscheck
-    if per.get()==2:
-        periodscheck = []
-        now = date.today()
-        temp = now
+    elif RadioB_Sellector.get()==2:
         delta = timedelta(weeks = 4.4)
-        for i in range(4):
-            string = ''
-            string = temp.strftime("%d/%m/%Y") + ' - '
-            temp = temp - delta
-            string += datetime.strftime(temp, "%d/%m/%Y")
-            periodscheck.append(string)
-        ch_period['values'] = periodscheck
-    if per.get()==3:
-        periodscheck = []
-        now = date.today()
-        temp = now
+    elif RadioB_Sellector.get() == 3:
         for i in range(4):
             ost = int(datetime.strftime(temp, "%m")) % 3
             delta = timedelta(weeks=ost * 4.4)
-            string = ''
             temp = temp - delta
             string = temp.strftime("%d/%m/%Y") + ' - '
             delta = timedelta(weeks=13.2)
             temp = temp - delta
             string += datetime.strftime(temp, "%d/%m/%Y")
             periodscheck.append(string)
-        ch_period['values'] = periodscheck
-    if per.get()==4:
-        periodscheck = []
-        now = date.today()
-        temp = now
+        CB_Periods['values'] = periodscheck
+        flag = False
+    elif RadioB_Sellector.get()==4:
         delta = timedelta(weeks = 52)
+
+    if flag:
         for i in range(4):
-            string = ''
             string = temp.strftime("%d/%m/%Y") + ' - '
             temp = temp - delta
-            string += datetime.strftime(temp, "%d/%m/%Y")
+            string+=datetime.strftime(temp,"%d/%m/%Y")
             periodscheck.append(string)
-        ch_period['values'] = periodscheck
+        CB_Periods['values'] = periodscheck
 
+def convert_value():
+    x = CB_Country1.get()
+    y = CB_Country2.get()
+    z = ToConvertField_Entry.get()
 
+    x = Valutues[Countries.index(x)]
+    y = Valutues[Countries.index(y)]
 
-
-
-
-
-
-def convert():
-    x = country_1.get()
-    y = country_2.get()
-    z = money.get()
-    x_index = countries.index(x)
-    x1 = valutues[x_index]
-    y_index = countries.index(y)
-    y1 = valutues[y_index]
-    x1 = x1.replace(',','.')
-    y1 = y1.replace(',', '.')
-    x = float(x1)
-    y = float(y1)
+    x = float(x.replace(',','.'))
+    y = float(y.replace(',', '.'))
     z = float(z)
-    res = str(round(float((x*z)/y),2))
-    result.configure(text = res)
+
+    res = "Converted: " + str(round(float((x*z)/y),2))
+    ConvertedField_Label.configure(text = res)
 
 
+"""
+Описываем весь Layout
+"""
+Countries = []
+Valutues = []
 
-window = Tk()
-window.title("Конвертер валют")
-window.minsize(width=500, height=200)
-window.maxsize(width=1600, height=1000)
-tab_control = Notebook(window)
-calc = Frame(tab_control)
-tab_control.add(calc, text="Калькулятор валют")
+App = Tk()
+Controller = Notebook(App)
+Calculator = Frame(Controller)
+CB_Country1 = Combobox(Calculator)
+CB_Country2 = Combobox(Calculator)
+Grafic = Frame(Controller)
+CB_Country3 = Combobox(Grafic)
 
-countries = []
-valutues = []
-countries = parsing(datetime.today())[0]
-country_1 = Combobox(calc)
-country_1.grid(row=0, column=0, padx=10, pady=10, ipadx=25)
-country_1['values'] = countries
-country_2 = Combobox(calc)
-country_2.grid(row=1, column=0, padx=10, pady=10, ipadx=25)
-country_2['values'] = countries
-money = Entry(calc)
-money.grid(row=0, column=1, pady=10, padx=10)
-result = Label(calc, text="")
-result.grid(row=1, column=1, pady=10, padx=10)
-convert = Button(calc, text = "Конвертировать",command = convert)
-convert.grid(row = 0, column = 2, padx = 10, pady = 10, ipadx = 15)
+ToConvertField_Entry = Entry(Calculator)
+ConvertedField_Label = Label(Calculator, text="")
+RadioB_Sellector = IntVar()
 
-grafic = Frame(tab_control)
-tab_control.add(grafic, text = "Динамика курса")
-set1 = Label(grafic, text = "Валюта")
-set1.grid(row = 0, column = 0, ipadx = 25)
-set2 = Label(grafic, text = "Период")
-set2.grid(row = 0, column = 1, ipadx = 25)
-set3 = Label(grafic, text = "Выбор периода")
-set3.grid(row = 0, column = 2, ipadx = 25)
-country_3 = Combobox(grafic)
-country_3.grid(row=1, column=0, padx=10, pady=10, ipadx=25)
-country_3['values'] = countries
-per = IntVar()
-periodscheck=[]
-period1 = Radiobutton(grafic, text = 'Неделя', value = 1,variable= per,command=period)
-period1.grid(row = 1, column = 1)
-period2 = Radiobutton(grafic, text = 'Месяц', value = 2,variable= per, command=period)
-period2.grid(row = 2, column = 1)
-period3 = Radiobutton(grafic, text = 'Квартал', value = 3,variable= per,command=period)
-period3.grid(row = 3, column = 1, pady = 10)
-period4 = Radiobutton(grafic, text = 'Год', value = 4,variable= per, command=period)
-period4.grid(row = 4, column = 1)
-ch_period = Combobox(grafic)
-ch_period.grid(row=1, column=2, padx=10, pady=10, ipadx=25,)
-draw = Button(grafic, text = "Нарисовать график!",command = graf)
-draw.grid(row = 4, column = 0, padx = 10, pady = 10, ipadx = 15)
+CB_Periods = Combobox(Grafic)
+Button_Draw = Button(Grafic, text="Нарисовать график!", command=print_graf)
+Button_Convert = Button(Calculator, text="Конвертировать", command=convert_value)
 
 
+def main():
 
-tab_control.pack(expand=True, fill=BOTH, side="top")
-window.mainloop()
+    App.title("Конвертер валют")
+    App.minsize(width=500, height=200)
+    App.maxsize(width=1600, height=1000)
+
+
+    Controller.add(Calculator, text="Калькулятор валют")
+
+    Countries = parsing_countries(datetime.today())[0]
+
+    CB_Country1.grid(row=0, column=0, padx=10, pady=10, ipadx=25)
+    CB_Country1['values'] = Countries
+
+    CB_Country2.grid(row=1, column=0, padx=10, pady=10, ipadx=25)
+    CB_Country2['values'] = Countries
+
+    ToConvertField_Entry.grid(row=0, column=1, pady=10, padx=10)
+    ConvertedField_Label.grid(row=1, column=1, pady=10, padx=10)
+    Button_Convert.grid(row = 0, column = 2, padx = 10, pady = 10, ipadx = 15)
+
+    Controller.add(Grafic, text ="Динамика курса")
+    Value_Label = Label(Grafic, text ="Валюта")
+    Value_Label.grid(row = 0, column = 0, ipadx = 25)
+    RBPeriod_Label = Label(Grafic, text ="Период")
+    RBPeriod_Label.grid(row = 0, column = 1, ipadx = 25)
+    CBPeriod_Label = Label(Grafic, text ="Выбор периода")
+    CBPeriod_Label.grid(row = 0, column = 2, ipadx = 25)
+
+    CB_Country3.grid(row=1, column=0, padx=10, pady=10, ipadx=25)
+    CB_Country3['values'] = Countries
+
+    RB_WeekPeriod = Radiobutton(Grafic, text='Неделя', value=1, variable=RadioB_Sellector, command=sellect_period)
+    RB_WeekPeriod.grid(row = 1, column = 1)
+    RB_MonthPeriod = Radiobutton(Grafic, text ='Месяц', value = 2, variable= RadioB_Sellector, command=sellect_period)
+    RB_MonthPeriod.grid(row = 2, column = 1)
+    RB_QuoterPeriod = Radiobutton(Grafic, text ='Квартал', value = 3, variable= RadioB_Sellector, command=sellect_period)
+    RB_QuoterPeriod.grid(row = 3, column = 1, pady = 10)
+    RB_YearPeriod = Radiobutton(Grafic, text ='Год', value = 4, variable= RadioB_Sellector, command=sellect_period)
+    RB_YearPeriod.grid(row = 4, column = 1)
+
+    CB_Periods.grid(row=1, column=2, padx=10, pady=10, ipadx=25, )
+    Button_Draw.grid(row = 3, column = 2, padx = 10, pady = 10, ipadx = 15)
+
+    Controller.pack(expand=True, fill=BOTH, side="top")
+    App.mainloop()
+
+if __name__=='__main__':
+    main()
